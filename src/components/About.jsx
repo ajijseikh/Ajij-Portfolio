44fd4a9f76e312cd252b505abace266d4d25ae78
ajijsk <ajijsk240@gmail.com>
2023-05-18 20:54:53 +0530

    modify css-2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,12 +7,12 @@ const About = () => {
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+        <div className="pb-8 mt-8">
+          <p className="text-4xl font-bold  border-b-4 border-gray-500 p-1 inline">
             About
           </p>
         </div>
-        <p className="text-xl mt-20">
+        <p className="text-xl mt-2">
           Hello World! I'm <b>Ajij Seikh</b> from West Bengal , a Full-Stack Web
           Developer.I love combining the worlds of logic and creative design
           to make eye-catching, accessible, and user-friendly websites and
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -70,12 +70,13 @@ const Experience = () => {
       className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white ">
-        <div>
+        <div className="mt-8">
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline ">
             Experience
           </p>
-          <p className="py-6 ">These are the technologies I've worked with</p>
-        </div>
+          </div>
+          <p className="py-4 ">These are the technologies I've worked with</p>
+        
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8  px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
